fe9dbe3ebd3b71e194d111930857333c9d4b81a2
yusrayasar <yusrayasar77@gmail.com>
2023-05-08 16:33:51 +0530

    code

---
+++ pest prediction/pest prediction/db.sql
@@ -0,0 +1,212 @@
+/*
+SQLyog Community v13.0.1 (64 bit)
+MySQL - 5.5.20-log : Database - pest detection
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pest detection` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `pest detection`;
+
+/*Table structure for table `complaint` */
+
+DROP TABLE IF EXISTS `complaint`;
+
+CREATE TABLE `complaint` (
+  `Comp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `F_id` int(11) DEFAULT NULL,
+  `ex_id` int(11) DEFAULT NULL,
+  `Complaint` varchar(200) DEFAULT NULL,
+  `Reply` varchar(200) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  PRIMARY KEY (`Comp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `complaint` */
+
+insert  into `complaint`(`Comp_id`,`F_id`,`ex_id`,`Complaint`,`Reply`,`Date`) values 
+(1,4,2,'That is not the correct pest','sorry ','2023-03-28');
+
+/*Table structure for table `crop` */
+
+DROP TABLE IF EXISTS `crop`;
+
+CREATE TABLE `crop` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT,
+  `crop_name` varchar(200) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `crop` */
+
+insert  into `crop`(`c_id`,`crop_name`,`description`) values 
+(2,'corn','best crop in India');
+
+/*Table structure for table `doubt` */
+
+DROP TABLE IF EXISTS `doubt`;
+
+CREATE TABLE `doubt` (
+  `D_id` int(11) NOT NULL AUTO_INCREMENT,
+  `F_id` int(11) DEFAULT NULL,
+  `ex_id` int(11) DEFAULT NULL,
+  `Doubt` varchar(200) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Reply` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`D_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `doubt` */
+
+insert  into `doubt`(`D_id`,`F_id`,`ex_id`,`Doubt`,`Date`,`Reply`) values 
+(1,4,2,'Is this fertilizer is suitable?','2023-03-28','yes');
+
+/*Table structure for table `expert` */
+
+DROP TABLE IF EXISTS `expert`;
+
+CREATE TABLE `expert` (
+  `ex_id` int(11) NOT NULL AUTO_INCREMENT,
+  `l_id` int(11) DEFAULT NULL,
+  `First_name` varchar(200) DEFAULT NULL,
+  `Last_name` varchar(200) DEFAULT NULL,
+  `Phone_no` varchar(200) DEFAULT NULL,
+  `Place` varchar(200) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Pin` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`ex_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `expert` */
+
+insert  into `expert`(`ex_id`,`l_id`,`First_name`,`Last_name`,`Phone_no`,`Place`,`Age`,`Pin`) values 
+(1,2,'Ashique','Ash','9874563211','Wayanad',23,670731);
+
+/*Table structure for table `farmer` */
+
+DROP TABLE IF EXISTS `farmer`;
+
+CREATE TABLE `farmer` (
+  `F_id` int(11) NOT NULL AUTO_INCREMENT,
+  `l_id` int(11) DEFAULT NULL,
+  `First_name` varchar(200) DEFAULT NULL,
+  `Last_name` varchar(200) DEFAULT NULL,
+  `Place` varchar(200) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Pin` bigint(20) DEFAULT NULL,
+  `Phone_no` bigint(25) DEFAULT NULL,
+  PRIMARY KEY (`F_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Data for the table `farmer` */
+
+insert  into `farmer`(`F_id`,`l_id`,`First_name`,`Last_name`,`Place`,`Age`,`Pin`,`Phone_no`) values 
+(1,4,'Yusra','Yasar','Nadapuram',22,673503,9562918880),
+(2,5,'Nusreen','Fathima','Kallachi',22,673504,2147483647),
+(3,6,'Rabeeh','Rahman','Vengara',22,614257,9632145872),
+(4,7,'Nihad','Abdulla','Wayanad',21,673503,9630852741);
+
+/*Table structure for table `feedback` */
+
+DROP TABLE IF EXISTS `feedback`;
+
+CREATE TABLE `feedback` (
+  `Feed_id` int(11) NOT NULL AUTO_INCREMENT,
+  `l_id` int(11) DEFAULT NULL,
+  `Feedback` varchar(200) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  PRIMARY KEY (`Feed_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `feedback` */
+
+insert  into `feedback`(`Feed_id`,`l_id`,`Feedback`,`Date`) values 
+(1,4,'Godd website','2023-03-28');
+
+/*Table structure for table `fertilizer` */
+
+DROP TABLE IF EXISTS `fertilizer`;
+
+CREATE TABLE `fertilizer` (
+  `Fert_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ex_id` int(11) DEFAULT NULL,
+  `Fertilizer name` varchar(200) DEFAULT NULL,
+  `Description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`Fert_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `fertilizer` */
+
+insert  into `fertilizer`(`Fert_id`,`ex_id`,`Fertilizer name`,`Description`) values 
+(1,1,'cow dung manuare','best fertilizer ');
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `Type` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`id`,`username`,`password`,`Type`) values 
+(1,'admin','admin','agriculture officer'),
+(2,'ashiq','123456','expert'),
+(3,'niala','niala@123','expert'),
+(4,'yus','yus@123','farmer'),
+(5,'nuchii','nuchi@1234','farmer'),
+(6,'Rabee','rabe@123','farmer'),
+(7,'Nihad','nihu@123','reject');
+
+/*Table structure for table `notification` */
+
+DROP TABLE IF EXISTS `notification`;
+
+CREATE TABLE `notification` (
+  `N_id` int(11) NOT NULL AUTO_INCREMENT,
+  `l_id` int(11) DEFAULT NULL,
+  `Notification` varchar(200) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  PRIMARY KEY (`N_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `notification` */
+
+insert  into `notification`(`N_id`,`l_id`,`Notification`,`Date`) values 
+(1,1,'no notification','2023-03-28');
+
+/*Table structure for table `tips` */
+
+DROP TABLE IF EXISTS `tips`;
+
+CREATE TABLE `tips` (
+  `Tip_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ex_id` int(11) DEFAULT NULL,
+  `Tips` varchar(200) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  PRIMARY KEY (`Tip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tips` */
+
+insert  into `tips`(`Tip_id`,`ex_id`,`Tips`,`Date`) values 
+(1,1,'check for cracks and gaps','2023-03-28');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ pest prediction/pest prediction/src/coding.py
@@ -0,0 +1,621 @@
+import functools
+from flask import *
+from src.dbconnectionnew import *
+from src.cnn_pest import *
+
+app = Flask(__name__)
+app.secret_key="1234"
+
+
+def login_required(func):
+    @functools.wraps(func)
+    def secure_function():
+        if "lid" not in session:
+            return render_template('loginindex.html')
+        return func()
+
+    return secure_function
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('loginindex.html')
+
+@app.route('/')
+def login():
+    return render_template("loginindex.html")
+
+
+@app.route('/logincode',methods=['post'])
+def login_code():
+    username=request.form['textfield']
+    password=request.form['textfield2']
+    qry="select * from login where username=%s and password=%s"
+    val=(username,password)
+    res=selectone(qry,val)
+    if res is None:
+        return '''<script>alert("invalid");window.location='/'</script>'''
+    elif res['Type']=='agriculture officer':
+
+        session['lid'] = res['id']
+        return redirect('/ag_home')
+
+    elif res['Type']=='expert':
+        session['lid']=res['id']
+        return redirect('/expert_homepage')
+    elif res['Type']=='farmer':
+        session['lid'] = res['id']
+        return redirect('/farmer_home')
+
+    else:
+        return '''<script>alert(" invalid ");window.location='/complaints'</script>'''
+
+@app.route('/register')
+def register():
+    return render_template("regindex.html")
+
+
+@app.route('/register1',methods=['post'])
+def register1():
+    fname = request.form['textfield']
+    lname = request.form['textfield2']
+    age = request.form['textfield3']
+    place = request.form['textfield4']
+    pin= request.form['textfield5']
+    phoneno = request.form['textfield6']
+    username = request.form['textfield7']
+    password = request.form['textfield8']
+    qry="insert into login values(null,%s,%s,'pending')"
+    val=(username,password)
+    id=iud(qry,val)
+    qry="insert into farmer values(null,%s,%s,%s,%s,%s,%s,%s)"
+    val=(str(id),fname,lname,place,age,pin,phoneno)
+    iud(qry,val)
+    return '''<script>alert(" registered successully");window.location='/'</script>'''
+
+
+
+
+
+@app.route('/ag_home')
+def ag_home():
+    return render_template("Agriculture Officer/aoindex.html")
+
+
+@app.route('/add_crop',methods=['post','get'])
+@login_required
+def add_crop():
+    return render_template("Agriculture Officer/Add Crop.html")
+
+
+@app.route('/add_expert',methods=['post','get'])
+@login_required
+def add_expert():
+    return render_template("Agriculture Officer/Add Experts.html")
+
+
+@app.route('/add_notification')
+@login_required
+def add_notification():
+    qry="SELECT * FROM notification WHERE l_id=%s"
+    res=selectall2(qry,session['lid'])
+    return render_template("Agriculture Officer/Add Notification.html",val=res)
+
+
+@app.route('/manage_crop')
+@login_required
+def manage_crop():
+    qry="select * from crop"
+    res=selectall(qry)
+    return render_template("Agriculture Officer/Manage Crop.html",val=res)
+
+@app.route('/delete_crop')
+@login_required
+def delete_crop():
+    id=request.args.get('id')
+    qry="delete FROM crop WHERE c_id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully deleted");window.location='/manage_crop'</script>'''
+
+
+@app.route('/edit_crop')
+@login_required
+def edit_crop():
+    id=request.args.get('id')
+    qry="select * FROM crop WHERE c_id=%s"
+    res=selectone(qry,id)
+    session['cid']=id
+    return render_template("Agriculture Officer/Edit crop.html",val=res)
+
+@app.route('/update_crop',methods=['post'])
+@login_required
+def update_crop():
+    crop=request.form['textfield']
+    description=request.form['textfield2']
+    cid=session['cid']
+    qry="UPDATE `crop` SET `crop_name`=%s,`description`=%s WHERE c_id=%s"
+    val=(crop,description,cid)
+    iud(qry,val)
+    return '''<script>alert(" Successfully edited");window.location='/manage_crop'</script>'''
+
+
+
+
+@app.route('/manage_expert')
+@login_required
+def manage_expert():
+    qry="SELECT * from expert"
+    res=selectall(qry)
+    return render_template("Agriculture Officer/Manage Expert.html",val=res)
+
+@app.route('/delete_expert')
+@login_required
+def delete_expert():
+    id=request.args.get('id')
+    qry="delete FROM expert WHERE ex_id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully deleted");window.location='/manage_expert'</script>'''
+
+@app.route('/edit_expert')
+@login_required
+def edit_expert():
+    id=request.args.get('id')
+    qry="select * FROM expert WHERE ex_id=%s"
+    res=selectone(qry,id)
+    session['exid']=id
+    return render_template("Agriculture Officer/Edit expert.html",val=res)
+
+@app.route('/update_expert',methods=['post'])
+@login_required
+def update_expert():
+    Fname=request.form['textfield']
+    Lname=request.form['textfield2']
+    Age = request.form['textfield3']
+    Place = request.form['textfield4']
+    Pin = request.form['textfield5']
+    Phoneno = request.form['textfield6']
+
+    exid=session['exid']
+    qry="UPDATE `expert` SET `First_name`=%s,`Last_name`=%s,`Phone_no`=%s,`Place`=%s,`Age`=%s,`Pin`=%s WHERE ex_id=%s"
+    val=(Fname,Lname,Phoneno,Place,Age,Pin,exid)
+    iud(qry,val)
+    return '''<script>alert(" Successfully edited");window.location='/manage_expert'</script>'''
+
+
+
+
+@app.route('/send_notification',methods=['get','post'])
+@login_required
+def send_notification():
+    return render_template("Agriculture Officer/Send Notifications.html")
+
+@app.route('/delete_notification')
+@login_required
+def delete_notification():
+    id=request.args.get('id')
+    qry="delete FROM notification WHERE N_id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully deleted");window.location='/add_notification#about-us'</script>'''
+
+
+
+@app.route('/verify_farmer')
+@login_required
+def verify_farmer():
+    qry="SELECT * FROM `farmer` JOIN `login` ON `login`.`id`=`farmer`.`l_id` "
+    res=selectall(qry)
+    return render_template("Agriculture Officer/Verify Farmer.html",val=res)
+
+@app.route('/accept_farmer')
+@login_required
+def accept_farmer():
+    id = request.args.get('id')
+    qry="UPDATE `login` SET `Type`='farmer' WHERE id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully accepted");window.location='/verify_farmer#about-us'</script>'''
+
+
+
+
+@app.route('/reject_farmer')
+@login_required
+def reject_farmer():
+    id = request.args.get('id')
+    qry = "UPDATE `login` SET `Type`='reject' WHERE id=%s"
+    iud(qry, id)
+    return '''<script>alert(" Successfully rejected");window.location='/verify_farmer#about-us'</script>'''
+
+
+
+@app.route('/view_feedback')
+@login_required
+def view_feedback():
+    qry ="SELECT farmer.First_name,farmer.Last_name,feedback.* FROM farmer JOIN feedback ON farmer.l_id=feedback.l_id"
+    res =selectall(qry)
+    return render_template("Agriculture Officer/View Feedback.html",val=res)
+
+
+@app.route('/view_registered_users')
+@login_required
+def view_registered_users():
+    qry = "select * from farmer"
+    res = selectall(qry)
+    return render_template("Agriculture Officer/View Registered Users.html",val=res)
+
+
+@app.route('/expert_homepage')
+def expert_homepage():
+    return render_template("Expert/exindex.html")
+
+
+@app.route('/add_fertilizer')
+@login_required
+def add_fertilizer():
+    return render_template("Expert/Add Fertilizer.html")
+
+
+@app.route('/add_tips')
+@login_required
+def add_tips():
+    return render_template("Expert/Add Tips.html")
+
+
+@app.route('/complaints')
+@login_required
+def complaints():
+    qry="SELECT farmer.First_name,farmer.Last_name,complaint.* FROM farmer JOIN `complaint` ON farmer.l_id=complaint.F_id WHERE ex_id=%s"
+    res=selectall2(qry,session['lid'])
+    return render_template("Expert/Complaints.html",val=res)
+
+
+@app.route('/doubt')
+@login_required
+def doubt():
+    qry=" SELECT farmer.First_name,farmer.Last_name,doubt.* FROM farmer JOIN doubt ON farmer.l_id=doubt.F_id WHERE ex_id=%s"
+    res = selectall2(qry, session['lid'])
+    return render_template("Expert/Doubt.html",val=res)
+
+
+@app.route('/manage_fertilizer')
+@login_required
+def manage_fertilizer():
+    qry="select * from fertilizer"
+    res=selectall(qry)
+    return render_template("Expert/Manage Fertilizer.html",val=res)
+
+@app.route('/delete_fertilizer')
+@login_required
+def delete_fertilizer():
+    id=request.args.get('id')
+    qry="delete FROM fertilizer WHERE Fert_id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully deleted");window.location='/manage_tips#about-us'</script>'''
+
+@app.route('/edit_fertilizer')
+@login_required
+def edit_fertilizer():
+    id=request.args.get('id')
+    qry="select *   FROM fertilizer WHERE Fert_id=%s"
+    res=selectone(qry,id)
+    session['fertid']=id
+    return render_template("Expert/Edit Fertilizer.html",val=res)
+
+
+
+@app.route('/update_Fertilizer',methods=['post'])
+@login_required
+def update_Fertilizer():
+    Fertname=request.form['textfield']
+    description=request.form['textfield2']
+    fertid=session['fertid']
+    qry="UPDATE `fertilizer` SET `Fertilizer name`=%s,`Description`=%s WHERE Fert_id=%s"
+    val=(Fertname,description,fertid)
+    iud(qry,val)
+    return '''<script>alert(" Successfully edited");window.location='/manage_fertilizer'</script>'''
+
+
+@app.route('/manage_tips')
+@login_required
+def manage_tips():
+    q="select * from tips"
+    v=selectall(q)
+    return render_template("Expert/Manage Tips.html",v=v)
+
+
+@app.route('/delete_tip')
+@login_required
+def delete_tip():
+    id=request.args.get('id')
+    qry="delete FROM tips WHERE Tip_id=%s"
+    iud(qry,id)
+    return '''<script>alert(" Successfully deleted");window.location='/manage_tips#about-us'</script>'''
+
+
+@app.route('/notification1')
+@login_required
+def notification1():
+    qry="SELECT * from Notification"
+    res=selectall(qry)
+    return render_template("Expert/Notification.html",val=res)
+
+
+@app.route('/farmer_home')
+def farmer_home():
+    return render_template("Farmer/faindex.html")
+
+
+@app.route('/complaints1')
+@login_required
+def complaints1():
+    qry="SELECT `complaint`.*,`expert`.`First_name`,`Last_name` FROM `expert` JOIN `complaint` ON `complaint`.`ex_id`=`expert`.`l_id` WHERE `complaint`.`F_id`=%s"
+    res=selectall2(qry,session['lid'])
+    return render_template("Farmer/Complaints.html",val=res)
+
+
+@app.route('/doubts1')
+@login_required
+def doubts1():
+    qry="SELECT `doubt`.*,`expert`.`First_name`,`Last_name` FROM `expert` JOIN `doubt` ON `doubt`.ex_id=`expert`.`l_id` WHERE `doubt`.F_id=%s"
+    res=selectall2(qry,session['lid'])
+    return render_template("Farmer/Doubts.html",val=res)
+
+
+@app.route('/feedback')
+@login_required
+def feedback():
+    qry = "SELECT * from feedback"
+    res = selectall(qry)
+
+    return render_template("Farmer/Feedback.html",val=res)
+
+
+@app.route('/pest_prediction')
+@login_required
+def pest_prediction():
+    return render_template("Farmer/Pest Prediction.html")
+
+
+@app.route('/pest_prediction1',methods=['post'])
+@login_required
+def pest_prediction1():
+    image=request.files['file']
+    image.save(r'C:\Users\hp\PycharmProjects\pest prediction\src\static\1.jpg')
+    result=predict('static/1.jpg')
+
+    print(result)
+    if result[0]==0:
+        res="aphids"
+    elif result[0]==1:
+        res="armyworm"
+    elif result[0]==2:
+        res="beetle"
+    elif result[0]==3:
+        res="bollworm"
+    elif result[0]==4:
+        res="caterpillars"
+    elif result[0]==5:
+        res="leaflets"
+    elif result[0]==6:
+        res="grasshopper"
+    elif result[0]==7:
+        res="mites"
+    elif result[0]==8:
+        res="mosquito"
+    elif result[0]==9:
+        res="sawfly"
+    elif result[0]==10:
+        res="stem_borer"
+    elif result[0]==11:
+        res="dryingofleaflets"
+    elif result[0]==12:
+        res="flaccidity"
+    else:
+        res="yellowing"
+
+    return render_template("Farmer/result.html",val=res)
+
+
+@app.route('/result',methods=['post'])
+def result():
+    return render_template("Farmer/result.html")
+
+
+
+@app.route('/send_complaints',methods=['post'])
+@login_required
+def send_complaints():
+    q="SELECT * FROM `expert`"
+    res=selectall(q)
+    return render_template("Farmer/send Complaints.html",val=res)
+
+
+@app.route('/send_complaints1',methods=['post'])
+@login_required
+def send_complaints1():
+    comp=request.form['textfield2']
+    eid=request.form['select']
+    q="INSERT INTO `complaint`   VALUES(NULL,%s,%s,%s,'pending',CURDATE())"
+    val=(session['lid'],eid,comp)
+    iud(q,val)
+    return '''<script>alert("Added");window.location='/complaints1'</script>'''
+
+
+
+@app.route('/send_doubts',methods=['post'])
+@login_required
+def send_doubts():
+    qry="select * from expert"
+    res=selectall(qry)
+    return render_template("Farmer/Send Doubts.html",val=res)
+
+@app.route('/send_doubt1',methods=['post'])
+@login_required
+def send_doubt1():
+    doubt=request.form['textfield2']
+    eid=request.form['select']
+    q="INSERT INTO `doubt`   VALUES(NULL,%s,%s,%s,curdate(),'pending')"
+    val=(session['lid'],eid,doubt)
+    iud(q,val)
+    return '''<script>alert("Added");window.location='/doubts1'</script>'''
+
+
+
+@app.route('/send_feedback',methods=['post'])
+@login_required
+def send_feedback():
+    return render_template("Farmer/Send Feedback.html")
+
+@app.route('/addfeedback',methods=['get','post'])
+@login_required
+def addfeedback():
+    feedback=request.form['textfield']
+    qry = "insert into feedback values(null,%s,%s,curdate())"
+    val = (session['lid'],feedback )
+    iud(qry, val)
+    return '''<script>alert("Added");window.location='/feedback '</script>'''
+
+
+
+
+
+
+@app.route('/view_crops')
+@login_required
+def view_crops():
+    qry="SELECT * from crop"
+    res=selectall(qry)
+    return render_template("Farmer/View Crops.html",val=res)
+
+
+@app.route('/view_fertilizer')
+@login_required
+def view_fertilizer():
+    qry="SELECT * from fertilizer"
+    res=selectall(qry)
+    return render_template("Farmer/View Fertilizer.html",val=res)
+
+
+@app.route('/view_notification')
+@login_required
+def view_notification():
+    qry="SELECT * from notification"
+    res=selectall(qry)
+    return render_template("Farmer/View Notification.html",val=res)
+
+
+@app.route('/view_tips')
+@login_required
+def view_tips():
+    qry="SELECT  expert.First_name,expert.Last_name,tips.* FROM expert JOIN tips ON expert.l_id=tips.ex_id"
+    res=selectall(qry)
+    return render_template("Farmer/View Tips.html",val=res)
+
+
+
+@app.route('/expertreg',methods=['get','post'])
+@login_required
+def expertreg():
+    fname=request.form['textfield']
+    lname=request.form['lname']
+    age=request.form['textfield2']
+    place=request.form['textfield4']
+    pin=request.form['textfield3']
+    phone_no=request.form['textfield5']
+    uname=request.form['textfield6']
+    pswrd=request.form['textfield7']
+    qry="insert into login values(null,%s,%s,'expert')"
+    val=(uname,pswrd)
+    id=iud(qry,val)
+    qry="insert into expert values(null,%s,%s,%s,%s,%s,%s,%s)"
+    val=(str(id),fname,lname,phone_no,place,age,pin)
+    iud(qry,val)
+    return'''<script>alert("Added");window.location='/manage_expert'</script>'''
+
+
+@app.route('/cropmanage',methods=['get','post'])
+@login_required
+def cropmanage():
+    cname=request.form['textfield']
+    discription=request.form['textfield2']
+    qry="insert into crop values(null,%s,%s)"
+    val=(cname,discription)
+    iud(qry,val)
+    return'''<script>alert("Added");window.location='/manage_crop'</script>'''
+
+@app.route('/addnotification1',methods=['get','post'])
+@login_required
+def addnotification1():
+    notification=request.form['textfield']
+    qry="insert into notification values(null,%s,%s,curdate())"
+    val=(session['lid'],notification)
+    iud(qry,val)
+    return'''<script>alert("Added");window.location='/add_notification'</script>'''
+
+
+@app.route('/addtips1',methods=['get','post'])
+@login_required
+def addtips1():
+    tips=request.form['textfield']
+    qry = "insert into tips values(null,%s,%s,curdate())"
+    val = (session['lid'], tips)
+    iud(qry, val)
+    return '''<script>alert("Added");window.location='/manage_tips'</script>'''
+
+@app.route('/addtips',methods=['get','post'])
+@login_required
+def addtips():
+    return render_template('Expert/Add Tips.html')
+
+@app.route('/addfertilizer1',methods=['get','post'])
+@login_required
+def addfertilizer1():
+    fertname=request.form['textfield2']
+    description=request.form['textfield']
+    qry = "insert into fertilizer values(null,%s,%s,%s)"
+    val = (session['lid'], fertname,description)
+    iud(qry, val)
+    return '''<script>alert("Added");window.location='/manage_fertilizer'</script>'''
+
+
+@app.route('/reply_to_doubt')
+@login_required
+def reply_to_doubt():
+    id=request.args.get('id')
+    session['did']=id
+    return render_template("Expert/Reply to Doubt.html")
+
+@app.route('/sendreply',methods=['get','post'])
+@login_required
+def sendreply():
+    reply=request.form['textfield']
+    qry = "UPDATE `doubt` SET Reply=%s WHERE D_id=%s"
+    val= ( reply,session['did'])
+    iud(qry, val)
+    return '''<script>alert("Added");window.location='/doubt'</script>'''
+
+
+
+@app.route('/reply_to_complaints')
+@login_required
+def reply_to_complaints():
+    id = request.args.get('id')
+    session['cid'] = id
+    return render_template("Expert/Reply to Complaints.html")
+
+@app.route('/sendreplytocomplaints',methods=['get','post'])
+@login_required
+def sendreplytocomplaints():
+    reply=request.form['textfield']
+    qry = "UPDATE `complaint` SET Reply=%s WHERE Comp_id=%s"
+    val= ( reply,session['cid'])
+    iud(qry, val)
+    return '''<script>alert("Reply Successfully");window.location='/complaints'</script>'''
+
+
+
+
+
+
+
+app.run(debug=True)
